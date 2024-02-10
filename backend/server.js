82a42506f390b845b445cacc88bc8858d10b477e
Manish <2002mybaran@gmail.com>
2024-02-10 21:35:12 +0530

    Database setup and models setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 const port = process.env.PORT | 5000;
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
+connectDB();
 const app = express();
 
 app.use('/api/users', userRoutes);
