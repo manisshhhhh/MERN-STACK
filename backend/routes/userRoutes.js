e9bc3b4ed74a22c5b48d72bd560f98835f5c46bc
Manish <2002mybaran@gmail.com>
2024-02-10 14:48:20 +0530

    routes and controller set up

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express'
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile, logoutUser, registerUser, updateUserProfile } from '../controllers/userController.js';
 
+router.post('/', registerUser);
 router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
