c1db0933121ca21de797e455d0cb06656ce96550
Manish <2002mybaran@gmail.com>
2024-02-10 12:05:01 +0530

    Adding of custom middleware and basic rotes and controller set up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,12 @@
+import asyncHandler from 'express-async-handler'
 //@desc   Auth user/ Set token
 //route   POST /api/users/auth
 //@access public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
+    // res.status(401);
+    // throw new Error('Somthing went wrong');
+
     res.status(200).json({ message: 'Auth user' });
-}
+});
 
 export { authUser };
