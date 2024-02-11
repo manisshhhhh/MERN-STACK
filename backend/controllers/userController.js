4ab0c3d22cf67620b55621a27d0f30e7cdd972e6
Manish <2002mybaran@gmail.com>
2024-02-11 12:30:13 +0530

    Generate jwt and save cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 //@desc   Auth user/ Set token
 //route   POST /api/users/auth
 //@access public
@@ -27,6 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
     });
 
     if (user) {
+        generateToken(res, user._id);
         res.status(201).json({
             _id: user._id,
             name: user.name,
